59d342fca4c9faca7934be790f8b09b7727084d2
Relarek <117584900+Relarek@users.noreply.github.com>
2023-01-03 21:49:43 +0300

    AppFuzy

---
+++ WindowsFormsAppFuzzy/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace WindowsFormsAppFuzzy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.button1.Location = new System.Drawing.Point(240, 486);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Температура в комнате";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(220)))), ((int)(((byte)(250)))));
+            chartArea1.AxisX.Maximum = 100D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(30, 30);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(819, 444);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            title1.Name = "Chart";
+            this.chart1.Titles.Add(title1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(90, 510);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(500, 497);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(500, 514);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(500, 530);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "FuzzyTest";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsAppFuzzy/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppFuzzy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Temp;
+            if (textBox1.Text == "")
+                return;
+
+            Temp = Convert.ToDouble(textBox1.Text);
+            //label1.Text = Temp.ToString();
+
+            TeaCold(Temp);
+            TeaWarm(Temp);
+            TeaHot(Temp);
+        }
+
+        public void TeaCold(double T)
+        {
+            if (T < Convert.ToDouble(chart1.Series[0].Points[1].XValue))
+            {
+                label1.Text = "В комнате холодно " + chart1.Series[0].Points[1].YValues[0].ToString();
+            }
+            else if ((T >= Convert.ToDouble(chart1.Series[0].Points[1].XValue)) &&
+                     (T <= Convert.ToDouble(chart1.Series[0].Points[2].XValue)))
+            {
+                label1.Text = "В комнате холодно " + ((chart1.Series[0].Points[2].XValue - T) * chart1.Series[0].Points[1].YValues[0] / 
+                    (chart1.Series[0].Points[2].XValue - chart1.Series[0].Points[1].XValue)).ToString();
+            }
+            else if (T > Convert.ToDouble(chart1.Series[0].Points[2].XValue))
+            {
+                label1.Text = "В комнате холодно " + chart1.Series[0].Points[2].YValues[0].ToString();
+            }
+            return;
+        }
+
+        public void TeaWarm(double T)
+        {
+            if (T < Convert.ToDouble(chart1.Series[1].Points[0].XValue))
+            {
+                label2.Text = "В комнате нормальная температура " + chart1.Series[1].Points[0].YValues[0].ToString();
+            }
+
+            else if ((T >= Convert.ToDouble(chart1.Series[1].Points[0].XValue)) &&
+                     (T <= Convert.ToDouble(chart1.Series[1].Points[1].XValue)))
+            {
+                label2.Text = "В комнате нормальная температура " + ((T - chart1.Series[1].Points[0].XValue) * chart1.Series[1].Points[1].YValues[0] /
+                    (chart1.Series[1].Points[1].XValue - chart1.Series[1].Points[0].XValue)).ToString();
+            }
+
+            else if ((T >= Convert.ToDouble(chart1.Series[1].Points[1].XValue)) &&
+                     (T <= Convert.ToDouble(chart1.Series[1].Points[2].XValue)))
+            {
+                label2.Text = "В комнате нормальная температура " + ((chart1.Series[1].Points[2].XValue - T) * chart1.Series[1].Points[1].YValues[0] /
+                    (chart1.Series[1].Points[2].XValue - chart1.Series[1].Points[1].XValue)).ToString();
+            }
+
+            else if (T > Convert.ToDouble(chart1.Series[1].Points[2].XValue))
+            {
+                label2.Text = "В комнате нормальная температура " + chart1.Series[1].Points[2].YValues[0].ToString();
+            }
+
+            return;
+        }
+
+        public void TeaHot(double T)
+        {
+            if (T < Convert.ToDouble(chart1.Series[2].Points[0].XValue))
+            {
+                label3.Text = "В комнате жарко " + chart1.Series[2].Points[0].YValues[0].ToString();
+            }
+            else if ((T >= Convert.ToDouble(chart1.Series[2].Points[0].XValue)) &&
+                     (T <= Convert.ToDouble(chart1.Series[2].Points[1].XValue)))
+            {
+                label3.Text = "В комнате жарко " + ((T - chart1.Series[2].Points[0].XValue ) * chart1.Series[2].Points[1].YValues[0] /
+                    (chart1.Series[2].Points[1].XValue - chart1.Series[2].Points[0].XValue)).ToString();
+            }
+            else if (T > Convert.ToDouble(chart1.Series[2].Points[1].XValue))
+            {
+                label3.Text = "В комнате жарко " + chart1.Series[2].Points[1].YValues[0].ToString();
+            }
+
+            return;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.BackColor = Color.Lime;
+
+            chart1.ChartAreas[0].BackColor = Color.White;
+             
+            // Series
+            chart1.Series.Add(new System.Windows.Forms.DataVisualization.Charting.Series());
+            chart1.Series.Add(new System.Windows.Forms.DataVisualization.Charting.Series());
+            chart1.Series.Add(new System.Windows.Forms.DataVisualization.Charting.Series());            
+
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+            // Series Color
+            chart1.Series[0].Color = Color.DarkGreen;
+            chart1.Series[1].Color = Color.Red;
+            chart1.Series[2].Color = Color.DarkBlue;
+
+            // LegendText
+            chart1.Series[0].LegendText = "Холодно";
+            chart1.Series[1].LegendText = "Комнатная температура";
+            chart1.Series[2].LegendText = "Жарко";
+
+            // Axis
+            chart1.ChartAreas[0].AxisY.Minimum = -5;
+            chart1.ChartAreas[0].AxisY.Maximum = 105;
+            chart1.ChartAreas[0].AxisY.Interval = 10;
+            chart1.ChartAreas[0].AxisY.IntervalOffset = 5;
+
+            chart1.ChartAreas[0].AxisX.Minimum = -10;
+            chart1.ChartAreas[0].AxisX.Maximum = 50;
+            chart1.ChartAreas[0].AxisX.Interval = 10;
+            chart1.ChartAreas[0].AxisX.IntervalOffset = 10;
+
+            // Points
+            chart1.Series[0].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint());
+            chart1.Series[0].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint());
+            chart1.Series[0].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint());
+
+            chart1.Series[0].Points[0].XValue = 0;
+            chart1.Series[0].Points[0].YValues[0] = 100;
+
+            chart1.Series[0].Points[1].XValue = 10;
+            chart1.Series[0].Points[1].YValues[0] = 100;
+
+            chart1.Series[0].Points[2].XValue = 18;
+            chart1.Series[0].Points[2].YValues[0] = 0;
+
+            chart1.Series[1].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint());
+            chart1.Series[1].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint());
+            chart1.Series[1].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint());
+
+            chart1.Series[1].Points[0].XValue = 16;
+            chart1.Series[1].Points[0].YValues[0] = 0;
+
+            chart1.Series[1].Points[1].XValue = 21;
+            chart1.Series[1].Points[1].YValues[0] = 100;
+
+            chart1.Series[1].Points[2].XValue = 25;
+            chart1.Series[1].Points[2].YValues[0] = 0;
+
+            chart1.Series[2].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint());
+            chart1.Series[2].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint());
+            chart1.Series[2].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint());
+
+            chart1.Series[2].Points[0].XValue = 23;
+            chart1.Series[2].Points[0].YValues[0] = 0;
+
+            chart1.Series[2].Points[1].XValue = 30;
+            chart1.Series[2].Points[1].YValues[0] = 100;
+
+            chart1.Series[2].Points[2].XValue = 40;
+            chart1.Series[2].Points[2].YValues[0] = 100;
+        }
+
+        /*http://www.cyberguru.ru/algorithms/algorithms-theory/algorithms-fuzzy-logic-system.html?showall=&start=0
+          http://www.cyberguru.ru/algorithms/algorithms-theory/algorithms-fuzzy-logic-system.html?showall=&start=1
+        */
+    }
+}
